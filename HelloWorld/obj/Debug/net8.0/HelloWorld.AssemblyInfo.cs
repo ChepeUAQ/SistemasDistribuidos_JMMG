6d19a8ffbc00a97d898fed1c12113851742040b6
ManuelMoralesG <114299979+ManuelMoralesG@users.noreply.github.com>
2024-09-04 21:14:22 -0600

    feat: UpdateUser

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd8dfd819f68c3fcb80d4a72dee28ebe0c13dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8cca6de000cefc9edec4846c9664484cef2fbdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
